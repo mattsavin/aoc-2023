54435738cea2f8cf630c74bbbe8db036ed780ba4
Matthew Savin <msavin1@sheffield.ac.uk>
2023-12-08 22:55:18 +0000

    day 2 part 1

--- day2/day2.py
+++ day2/day2.py
@@ -1,15 +1,25 @@
 from collections import defaultdict
-def part_one(data):
-    for i, j in enumerate(data):
-        data[i] = j.split(": ")
-        for k, l in enumerate(data[i]):
-            data[i][k] = l.split("; ")
-            for m, n in enumerate(data[i][k]):
-                data[i][k][m] = n.split(", ")
 
-    dictionary = defaultdict(lambda: defaultdict(data))
 
-    return dictionary
+lines = [x for x in open("day2.txt", "r").read().strip().split("\n")]
 
-data = open("day2.txt", "r").read().split("\n")
-print(part_one(data))
+def part1(lines):
+    total = 0
+    for lines in lines:
+        game_id = lines.split(":")[0].split(" ")[1]
+        lines = lines.split(":")[1]
+        possible = True
+        for draw in lines.split(";"):
+            d = defaultdict(int)
+            for result in draw.split(", "):
+                result = result.strip()
+                d[result.split(" ")[1]] += int(result.split(" ")[0])
+            if d["red"] > 12 or d["green"] > 13 or d["blue"] > 14:
+                possible = False
+
+        if possible:
+            total += int(game_id)
+
+    return total
+
+print(part1(lines))
