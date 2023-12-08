913f3d4a2f5b961d7bfc5a752a7e1529eb2b3558
Matthew Savin <msavin1@sheffield.ac.uk>
2023-12-08 23:10:32 +0000

    day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -22,4 +22,23 @@ def part1(lines):
 
     return total
 
-print(part1(lines))
+def part2(lines):
+    powersum = 0
+    for lines in lines:
+        game_id = lines.split(":")[0].split(" ")[1]
+        lines = lines.split(":")[1]
+        min_d = defaultdict(int)
+        for draw in lines.split(";"):
+            d = defaultdict(int)
+            for result in draw.split(", "):
+                result = result.strip()
+                d[result.split(" ")[1]] += int(result.split(" ")[0])
+            for key, data in d.items():
+                min_d[key] = max(min_d[key], data)
+        powersum += min_d["red"] * min_d["green"] * min_d["blue"]
+
+
+    return powersum
+
+#print(part1(lines))
+print(part2(lines))
