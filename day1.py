a8ffda2678bfd72087be1ba589870302c50aa84f
Matthew Savin <msavin1@sheffield.ac.uk>
2023-12-08 18:43:14 +0000

    day one refactor + complete part two

--- day1.py
+++ day1.py
@@ -1,22 +1,31 @@
-data = open("day1.txt", "r").read().split("\n")
-
-fullArray = []
-smallArray = []
-
-for s in data:
-    s = s.replace("one", "1")
-    s = s.replace("two", "2")
-    s = s.replace("three", "3")
-    s = s.replace("four", "4")
-    s = s.replace("five", "5")
-    s = s.replace("six", "6")
-    s = s.replace("seven", "7")
-    s = s.replace("eight", "8")
-    s = s.replace("nine", "9")
-    for t in s:
-        if t.isdigit():
-            smallArray.append(t)
-    fullArray.append(smallArray)
+def partOne(data):
+    fullArray = []
     smallArray = []
 
-print(sum(int(i[0] + i[len(i)-1]) for i in fullArray))
+    for line in data:
+        for character in line:
+            if character.isdigit():
+                smallArray.append(character)
+        fullArray.append(smallArray)
+        smallArray = []
+
+    print("Part one: ", sum(int(i[0] + i[-1]) for i in fullArray))
+
+
+def partTwo(data):
+    # Numbers can only be a part of another with the first or last letters
+    translation = {'zero': 'z0o', 'one': 'o1e', 'two': 't2o', 'three': 't3e',
+               'four': 'f4r', 'five': 'f5e', 'six': 's6x', 'seven': 's7n',
+               'eight': 'e8t', 'nine': 'n9e'}
+
+    total = 0
+    for line in data:
+        for key, value in translation.items():
+            line = line.replace(key, value)
+        numbers = [j for j in line if j.isdigit()]
+        total  += int(numbers[0] + numbers[-1])
+    print("Part two: ", total)
+
+data = open("day1.txt", "r").read().split("\n")
+partOne(data)
+partTwo(data)
