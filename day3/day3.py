fa09452545a18cc23af7c0437688028cc023ed53
Matthew Savin <msavin1@sheffield.ac.uk>
2023-12-09 00:26:11 +0000

    day 3 and 4

--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,16 @@
-lines = [x for x in open("input.txt", "r").read().strip().split("\n")]
+import math, re
 
-def part1(lines):
-    
-    pass
+board = list(open('input.txt'))
+chars = {(r, c): [] for r in range(140) for c in range(140)
+                    if board[r][c] not in '0123456789.'}
+
+for r, row in enumerate(board):
+    for n in re.finditer(r'\d+', row):
+        edge = {(r, c) for r in (r-1, r, r+1)
+                       for c in range(n.start()-1, n.end()+1)}
+
+        for o in edge & chars.keys():
+            chars[o].append(int(n.group()))
+
+print(sum(sum(p) for p in chars.values()))
+print(sum(math.prod(p) for p in chars.values() if len(p)==2))
--- day4/day4.py
+++ day4/day4.py
@@ -1,16 +1,32 @@
+from collections import defaultdict
+
 lines = [x for x in open("input.txt", "r").read().strip().split("\n")]
+
+def get_matching(line):
+    winning = set(map(int, list(filter(None, line.split(":")[1].split("|")[0].split(' ')))))
+    have = set(map(int, list(filter(None, line.split(":")[1].split("|")[1].split(' ')))))
+
+    return winning.intersection(have)
+
 
 def part1(lines):
     total = 0
     for line in lines:
-        winning = set(map(int, list(filter(None, line.split(":")[1].split("|")[0].split(' ')))))
-        have = set(map(int, list(filter(None, line.split(":")[1].split("|")[1].split(' ')))))
-
-        matched = winning.intersection(have)
+        matched = get_matching(line)
         total += ((2 ** (len(matched) - 1)) if len(matched) > 0 else 0)
 
     return total
-    
 
-print(part1(lines))
-#part1(lines)
+
+def part2(lines):
+    card_number_count = defaultdict(int)
+    for i, line in enumerate(lines):
+        card_number_count[i] += 1
+        matched = get_matching(line)
+
+        for j in range (len(matched)):
+            card_number_count[i+j+1] += card_number_count[i]
+
+    return sum(card_number_count.values())
+
+print(part2(lines))
